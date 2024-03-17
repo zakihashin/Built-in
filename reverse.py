59130447d2e2ddb6ad5eb05f039cd8c91968d1ca
zakihashin <155668242+zakihashin@users.noreply.github.com>
2024-03-17 17:35:50 +0000

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,16 @@
+def reverselist(listrev):
+    reverse = len(listrev) - 1
+
+    em = []
+
+    for f in range(reverse,-1,-1):
+    
+      em.append(listrev[f])
+    return em
+o = reverselist([3,4,6,1])
+print(o)
+
+
+
+    
+
